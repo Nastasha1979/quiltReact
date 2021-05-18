771fe42ca1e97001ef072db533c2ff83d7cd7d49
Nastasha Leach <nastasha.leach@outlook.com>
2021-05-18 15:55:10 -0500

    051821 4pm All dependencies and fonts added. Ready to go

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './Styles.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './fonts/Girassol/Girassol-Regular.ttf';
+import './fonts/Roboto/Roboto-Regular.ttf';
+import './fonts/Sacramento/Sacramento-Regular.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
